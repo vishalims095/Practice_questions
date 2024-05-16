4f9aa44157ff25046fd6a01bc3c3feb91d75f089
vishal sharma <36037597+vishalims095@users.noreply.github.com>
2024-05-16 21:21:13 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 1. Generator function : generator function is such type of function which execution can be paused  using yield keyword, to create generator function need to use "*" keyword after function, and generator function returns object instent of function.
-Ex : function* generatorFunc(i){
+Using next keyword it starts execution.
+	Ex : function* generatorFunc(i){
 	console.log("A")
   yield i;
   console.log('B')
